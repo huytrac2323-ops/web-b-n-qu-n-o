9e4f2f7ccee4158a2322c94bd15a3e9f628809b6
NhatHuy <huytrac2323@gmail.com>
2026-04-01 14:29:59 +0700

    update

--- Demo-master/Demo/Models/Product.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Demo.Models;
-
-public class Product
-{
-    public int Id { get; set; }
-    public string Name { get; set; }
-    public double Price { get; set; }
-    public string Image { get; set; }
-}
