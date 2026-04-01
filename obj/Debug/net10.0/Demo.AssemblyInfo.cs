f7694910724c67e86b490b44ae4887f0d43e6b6f
NhatHuy <huytrac2323@gmail.com>
2026-04-01 14:12:39 +0700

    clear

--- obj/Debug/net10.0/Demo.AssemblyInfo.cs
+++ obj/Debug/net10.0/Demo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Demo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a23540059c4b268fc56255798448b8765269f24f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d3b629c8bee68096f516300abbed3523d1dd921")]
 [assembly: System.Reflection.AssemblyProductAttribute("Demo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Demo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
